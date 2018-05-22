7caf301f45091399f9bd1ba5f29400af44cf4045
Camilo Zapata <ccamilozt@gmail.com>
2018-05-22 13:54:28 -0500

    add runLockVersion param

--- mbinit.go
+++ mbinit.go
@@ -26,19 +26,24 @@ import (
 var url string
 var podName string
 var buildVersion string
+var runLockVer string
 var step int
 
 func init() {
 	flag.StringVar(&url, "url", "mongodb://127.0.0.1:27017", "URL mongodb")
 	flag.StringVar(&podName, "podName", "localhost", "hostname POD")
 	flag.StringVar(&buildVersion, "buildVersion", "0.0.1", "Docker image build version")
-	flag.IntVar(&step, "step", 1, "step of the chek\n\t 1, lock/verify\n\t 2, wait\n\t 3, unlock\n\t")
+	flag.StringVar(&runLockVer, "runLockVer", "0.0.1", "run lock version")
+	flag.IntVar(&step, "step", 1, "step of the chek\n\t 1, lock/verify\n\t 2, wait\n\t 3, unlock\n\t 4, indexes\n\t")
 }
 
 type Podflag struct {
 	Id	int `bson:"_id"`
 	Name	string
+	Type	string
 	BuildVersion	string
+	RunLockVer	string
+	TimeStamp	int64
 }
 
 func main() {
@@ -56,11 +61,16 @@ func main() {
 	c := session.DB("test").C("initialize")
 
 
-	pod := Podflag{}
+	ts := time.Now().Unix()
+	pod := &Podflag{}
+	podLock := &Podflag{Id: 1, Name: podName, BuildVersion: buildVersion, RunLockVer: runLockVer, Type: "lock", TimeStamp: ts}
+	podUnLock := &Podflag{Id: 2, Name: podName, BuildVersion: buildVersion, RunLockVer: runLockVer, Type: "unlock", TimeStamp: ts}
+	podIndexes := &Podflag{Id: 3, Name: podName, BuildVersion: buildVersion, RunLockVer: runLockVer, Type: "indexes", TimeStamp: ts}
+
 	switch step {
 	case 1:
 		if err = c.FindId(1).One(&pod); err != nil {
-			if err = c.Insert(&Podflag{Id: 1, Name: podName, BuildVersion: buildVersion}); err != nil {
+			if err = c.Insert(podLock); err != nil {
 				log.Fatalln(err)
 			}
 			return
@@ -68,6 +78,13 @@ func main() {
 		if pod.Name == podName {
 			if err = c.FindId(2).One(&pod); err != nil {
 				return
+			}
+		}
+		if pod.RunLockVer != runLockVer {
+			if err = c.RemoveId(1); err == nil {
+				if err = c.Insert(podLock); err != nil {
+					return
+				}
 			}
 		}
 		log.Fatalln("the register already exists")
@@ -84,7 +101,9 @@ func main() {
 					return
 				}
 				if err = c.FindId(2).One(&pod); err == nil {
-					return
+					if pod.RunLockVer == runLockVer {
+						return
+					}
 				}
 			}
 		}
@@ -94,12 +113,29 @@ func main() {
 		}
 		if pod.Name == podName {
 			log.Printf("Remove lock: %v\n", pod.Name)
-			if _, err = c.UpsertId(2, &Podflag{Id: 2, Name: podName, BuildVersion: buildVersion}); err != nil {
+			if _, err = c.UpsertId(2, podUnLock); err != nil {
 				log.Fatalln(err)
 			}
 		}
+	case 4:
+		if err = c.FindId(1).One(&pod); err != nil {
+                        log.Fatalln(err)
+                }
+		if err = c.FindId(2).One(&pod); err != nil {
+                        log.Fatalln(err)
+		}
+		if err = c.FindId(3).One(&pod); err != nil {
+                        log.Println("Don't flag indexes")
+			pod.TimeStamp = 0
+                }
+		if ts > (pod.TimeStamp + 60*1) {
+			if _, err = c.UpsertId(3, podIndexes); err != nil {
+				log.Fatalln(err)
+			}
+			log.Println("Update flag indexes")
+		}
 	default:
-		log.Fatalln("Input valid step (1, 2 or 3)")
+		log.Fatalln("Input valid step (1, 2, 3 or 4)")
 	}
 }
 
